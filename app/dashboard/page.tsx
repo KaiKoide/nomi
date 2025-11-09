1d22df09439027a2ab77434fb196cc68c5c29ee6
Kai <kkai.chien0813@gmail.com>
2025-11-10 00:31:34 +0900

    feat: add card component

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardComponents } from "@/components/dashboard/dashboardComponents";
+
+export default function Dashboard() {
+  return (
+    <div className="bg-theme-dark h-screen">
+      <h1>ダッシュボード</h1>
+      <DashboardComponents />
+    </div>
+  );
+}
