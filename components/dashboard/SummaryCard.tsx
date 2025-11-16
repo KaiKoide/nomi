705346f32aa402de7352b06482559bd8bd9e2d85
Kai <kkai.chien0813@gmail.com>
2025-11-17 00:08:42 +0900

    feat: add card component

---
+++ components/dashboard/SummaryCard.tsx
@@ -0,0 +1,22 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const SummaryCard = () => {
+  return (
+    <Card className="bg-linear-to-br from-theme-dark to-theme-pink">
+      <CardHeader>
+        <CardTitle>合計金額</CardTitle>
+        <CardTitle className="text-4xl">￥12,800</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>次の支払日：　2025/12/01</CardDescription>
+        <CardDescription>登録中：　5件</CardDescription>
+      </CardContent>
+    </Card>
+  );
+};
