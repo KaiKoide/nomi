7672d181b24cfcc30beb0374cbf194db632df12d
Kai <kkai.chien0813@gmail.com>
2025-12-29 17:47:22 +0900

    chore: レビュー対応

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import prettierConfig from "eslint-config-prettier";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierConfig from 'eslint-config-prettier';
 
 const eslintConfig = [
   ...nextVitals,
   ...nextTs,
   prettierConfig,
   {
-    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+    ignores: [
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      'node_modules',
+    ],
   },
 ];
 
